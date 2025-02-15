4dae8fad589056f463f87691043cda92ffc9083e
Thomas Vandenbussche <thomas.vandenbussche@outlook.com>
2025-02-15 13:16:26 +0100

    add detail page and routing logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { NgModule }               from '@angular/core'
+import { RouterModule, Routes }   from '@angular/router'
+import { HomeComponent }          from './pages/home/home.component'
+import { NotFoundComponent }      from './pages/not-found/not-found.component'
+import { DetailComponent }        from './pages/detail/detail.component'
 
 const routes: Routes = [
   {
-    path: '',
+    path     : '',
     component: HomeComponent,
   },
   {
-    path: '**', // wildcard
+    path     : 'detail/:id',
+    component: DetailComponent,
+  },
+  {
+    path     : '**', // wildcard
     component: NotFoundComponent,
   },
 ];
@@ -18,4 +23,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
+
 export class AppRoutingModule {}
---
+++ src/app/pages/detail/detail.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { OlympicService } from 'src/app/core/services/olympic.service';
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: './detail.component.html',
+  styleUrls: ['./detail.component.scss'],
+})
+export class DetailComponent implements OnInit {
+  public olympics$: Observable<any> = of(null);
+
+  constructor(private olympicService: OlympicService) {}
+
+  ngOnInit(): void {
+    this.olympics$ = this.olympicService.getOlympics();
+  }
+}
